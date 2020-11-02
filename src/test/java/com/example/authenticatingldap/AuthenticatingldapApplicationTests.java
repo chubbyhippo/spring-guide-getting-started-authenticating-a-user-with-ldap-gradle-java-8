337a743777c6157658d1a7fcec81933929748bbe
ch <mkmsacc@outlook.com>
2020-11-02 19:53:33 +0700

    add unit tests for testing logins

--- src/test/java/com/example/authenticatingldap/AuthenticatingldapApplicationTests.java
+++ src/test/java/com/example/authenticatingldap/AuthenticatingldapApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.authenticatingldap;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,11 +14,25 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuthenticatingldapApplicationTests {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
 	public void loginWithValidUserThenAuthenticated() throws Exception {
+		FormLoginRequestBuilder login = formLogin().user("ben")
+				.password("benspassword");
+
+		mockMvc.perform(login).andExpect(authenticated().withUsername("ben"));
 	}
+
+	@Test
+	public void loginWithInvalidUserThenUnauthenticated() throws Exception {
+		FormLoginRequestBuilder login = formLogin().user("invalid")
+				.password("invalidpassword");
+
+		mockMvc.perform(login).andExpect(unauthenticated());
+
+	}
+
 }
